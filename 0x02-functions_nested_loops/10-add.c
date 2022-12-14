55403e6a0453a021ad86cf829cfe496d1c09808e
datsitsre <atsitsredaniel@outlook.com>
2022-12-14 01:04:03 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *add - add two number 
+  *Return: 0
+  */
+int add(int number1, int number2)
+{
+	int number1, number2, result;
+
+	result = number1 + number2;
+	return (result);
+}
