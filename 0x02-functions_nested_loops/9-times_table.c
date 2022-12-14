3ac2e0d12b72a07f08ee90657b7f0c6a55abbfa6
datsitsre <atsitsredaniel@outlook.com>
2022-12-14 00:55:25 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+  *times_table - print 9 times table
+  *Return: 0
+  */
+void times_table(void)
+{
+	int num_run, int multip1, int answer;
+
+	for (num_run = 0; num_run < 10; num_run++)
+	{
+		for (multip1 = 0; multip1 < 10; multip1++)
+		{
+			answer = multip1 * num_run;
+			if (multip1 == 0)
+			{
+				_putchar(answer + '0');
+			}
+			if (answer < 10 && multip1 != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(answer + '0');
+			}
+			else if (answer >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((answer / 10) + '0')
+				_putchar((answer % 10) + '0')
+			}
+		}
+		_putchar('\n');
+		}
+}
+
+
