f2b7cbb5d6eabff68fbaf303263b39c9e176df0f
datsitsre <atsitsredaniel@outlook.com>
2022-12-14 02:08:21 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,6 @@ void print_to_98(int n)
 		while (n > 98)
 		{
 			printf("%d, ", n--);
-			printf("%d\n", n);
 		}
 	}
 	else
@@ -21,6 +20,6 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n++);
 
-			printf("%d\n", n);
 		}
+			printf("%d\n", n);
 }
