d205904eefec06470b7c2ff1abca6cb19be2f074
datsitsre <atsitsredaniel@outlook.com>
2022-12-13 23:40:55 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
   *print_last_digit - print the last digit of a number
-  *@int: the number to give to a function
+  *@n: the number to give to a function
   *Return: 0
   */
 int print_last_digit(int n)
@@ -10,13 +10,9 @@ int print_last_digit(int n)
 	int  number;
 
 	if (n < 0)
-	{
 		n = -n;
 		number  = n % 10;
-	}
 	if (number < 0)
-	{
 		number = -number;
 		_putchar(number + '0');
-	}
 }
