16c8c1485be636c9e09b6fb14418d8a16c883071
datsitsre <atsitsredaniel@outlook.com>
2022-12-15 01:47:24 -0800

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *main - test the positivie or negative if it prints the correct output
+  *Return: 0
+  */
+int main(void)
+{
+	int i;
+
+	i = 98;
+
+	 positive_or_negative(i);
+
+	return (0);
+}
