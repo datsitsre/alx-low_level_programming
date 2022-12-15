ed4717f0f80f210db202675bb5694647f00aa161
datsitsre <atsitsredaniel@outlook.com>
2022-12-15 07:47:27 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *mul - multiply two numbers
+  *@a : first number 
+  *@b : second number
+  */
+int mul(int a, int c)
+{
+	int answer;
+
+	answer = a * b;
+	return (answer);
+
+}
