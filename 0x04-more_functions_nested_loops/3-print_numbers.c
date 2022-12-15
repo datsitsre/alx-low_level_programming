578836c3affeca0fa38ca676f11efcbd8edcdd4c
datsitsre <atsitsredaniel@outlook.com>
2022-12-15 08:48:14 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *print_numbers -  print numbers from 0 to 9
+  *Return: numbers
+  */
+void print_numbers(void)
+{
+
+	for (char x = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+		_putchar("\n");
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _isupper(int c);
 int mul(int a, int c);
 int _isdigit(int c);
-
+void print_numbers(void);
+int _putchar(char c);
 #endif
