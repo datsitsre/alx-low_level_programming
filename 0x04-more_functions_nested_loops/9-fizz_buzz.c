12820549e692afae72e3d5b71d17983febf9b223
datsitsre <atsitsredaniel@gmail.com>
2022-12-16 03:08:54 +0000

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *main - print 1  - 100 then 3 for Fizz 5 bizz 3 and 5 Fizzbuzz
+  *Return: 0
+  */
+int main(void)
+{
+	int index;
+
+	for (index = 1;  index <= 100; index++)
+	{
+		if (index % 3 == 0)
+			printf(" Fizz ");
+		else if (index % 5 == 0)
+			printf(" Buzz ");
+		else if (index % 3 == 0 && index % 5)
+			printf(" FizzBuzz ");
+		else
+			printf("%d ", index);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
