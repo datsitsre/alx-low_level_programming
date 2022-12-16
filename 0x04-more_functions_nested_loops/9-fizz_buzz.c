6e9825ce4d26c26717e73ed2c2e507abb8ef9ce9
datsitsre <atsitsredaniel@gmail.com>
2022-12-16 03:12:48 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (index = 1;  index <= 100; index++)
 	{
-		if (index % 3 == 0)
-			printf(" Fizz ");
+		if (index % 3 == 0 && index % 5 == 0)
+			printf(" FizzBuzz ");
 		else if (index % 5 == 0)
 			printf(" Buzz ");
-		else if (index % 3 == 0 && index % 5)
-			printf(" FizzBuzz ");
+		else if (index % 3 == 0)	
+			printf(" Fizz ");
 		else
 			printf("%d ", index);
 	}
