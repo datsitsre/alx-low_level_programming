deacb69ca465475a292ca15d79028dd5dacd993b
datsitsre <atsitsredaniel@gmail.com>
2022-12-16 12:32:40 +0000

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  *print_triangle - print a triangle on the screen
+  *@size : size of the triangle
+  *Return : 0
+  */
+void print_triangle(int size)
+{
+	int index;
+	int index2;
+
+	for (index = 1; index <= size; index++)
+	{
+		for (index2 = index; index2 <= size; index2++)
+		{
+			_putchar(' ');
+		}
+		for (index2 = 1; index2 <= index; index2++)
+		{
+			_putchar('*');
+		}
+		_putchar('\n');
+	}
+}
+
