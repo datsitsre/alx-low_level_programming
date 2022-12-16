9a64d1272f051171929e7fd9a4984c6ed4a1f1b7
datsitsre <atsitsredaniel@gmail.com>
2022-12-16 12:46:26 +0000

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,8 +10,11 @@ void print_triangle(int size)
 	int index;
 	int index2;
 
+	if (size <= 0)
+		_putchar('\n');
 	for (index = 1; index <= size; index++)
 	{
+
 		for (index2 = index; index2 <= size; index2++)
 		{
 			_putchar(' ');
