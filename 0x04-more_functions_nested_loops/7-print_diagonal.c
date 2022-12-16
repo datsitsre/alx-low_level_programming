c33dc7f47da2bfec63db79e2e8f4541675193a20
datsitsre <atsitsredaniel@outlook.com>
2022-12-15 16:11:30 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,6 +3,7 @@
 /**
   *print_diagonal - print a diagonAL
   *@n: the number of times the diagonal will go
+  *Return: 0 if n = 0
   */
 void print_diagonal(int n)
 {
@@ -10,7 +11,7 @@ void print_diagonal(int n)
 
 	for (index = 0; index < n; index++)
 	{
-		if (n < 0)
+		if (n <= 0)
 			_putchar('\n');
 		_putchar('\\');
 	}
