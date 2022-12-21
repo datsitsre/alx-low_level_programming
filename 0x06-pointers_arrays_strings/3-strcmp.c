33b9fd49cb6dda3ef84b80a838f4a2f0a8ecf96b
datsitsre <atsitsredaniel@gmail.com>
2022-12-21 23:37:22 +0000

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  *_strcmp - compares two strings
+  *@s1: first string
+  *@s2: second string
+  *Return: value
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int len;
+	int result;
+
+	for (len = 0; s1[len] != '\0' || s2[len] != '\0'; len++)
+	{
+		if (s1[len] > s2[len])
+		{
+			result = 15;
+		}
+		else if (s1[len] < s2[len])
+		{
+			result = -15;
+		}
+		else
+			result = 0;
+	}
+	return (result);
+
+}
+
