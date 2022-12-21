e176b3897efdfac517951474ccd2fab04029176a
datsitsre <atsitsredaniel@gmail.com>
2022-12-21 22:24:11 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *_strncpy - copies a string
+  *@dest : destination
+  *@src: source string
+  *@n: the number of string to cpy
+  *Return: des
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
+
+	return (dest);
+
+}
