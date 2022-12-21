84ad6d323802e909916ad5feefbada27bc64f1a8
datsitsre <atsitsredaniel@gmail.com>
2022-12-21 21:17:36 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,16 +11,15 @@ char *_strcat(char *dest, char *src)
 	int len = 0;
 	int index;
 
-	while (*dest)
+	while (dest[len] != '\0')
 	{
 		len++;
-		dest++;
 	}
 
 
 
 	for (index = 0; src[index] != '\0'; index++)
 		dest[len + index] = src[index];
-	dest[len + index] = '\0';
+
 	return (dest);
 }
