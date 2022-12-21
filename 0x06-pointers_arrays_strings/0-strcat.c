e50d02b12808935799d8b9e638c16588b66c541f
datsitsre <atsitsredaniel@gmail.com>
2022-12-21 11:30:32 +0000

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *_strcat - concatenates two strings
+  *@dest: destination 
+  *@src: source 
+  */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int index;
+
+	while (*dest)
+	{
+		len++;
+		dest++;
+	}
+
+
+
+	for (index = 0;src[index] != '\0';index++)
+	{
+		dest[len + index] = src[index];
+		dest[len + index] = '\0';
+	}
+	return (dest);
+}
