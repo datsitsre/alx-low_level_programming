ecca8c9ea43cb119ccea7adfc6d57e7969e887f0
datsitsre <atsitsredaniel@gmail.com>
2022-12-22 23:29:10 +0000

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,7 +14,8 @@ int *p;
 a[2] = 1024;
 p = &n; /* * - you are not allowed to use a
 * - you are not allowed to modify p
-* - only one statement */
+* - only one statement
+	 */
 *(p + 2) = 98;/*
 * - you are not allowed to code anything else than this line of code
 */
