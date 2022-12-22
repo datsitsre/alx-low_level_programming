665f996a05c4127fc40796ef675c10a6cd7e97a0
datsitsre <atsitsredaniel@gmail.com>
2022-12-22 12:09:57 +0000

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *string_toupper - to upper case
+  *Return: a string
+  */
+char *string_toupper(char *c)
+{
+	int index;
+
+	for (index = 0; c[index] != '\0'; index++)
+	{
+		if (c[index] >= 'a' && c[index] <='z')
+			c[index] = c[index] - 32;
+	}
+	return (c);
+}
