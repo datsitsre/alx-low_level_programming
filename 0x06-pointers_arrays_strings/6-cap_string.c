029cbd39ab2dcfed91e4c65fdbc3b89273f7ff67
datsitsre <atsitsredaniel@gmail.com>
2022-12-22 19:16:57 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,26 +3,26 @@
 /**
   *cap_string - captalize a string
   *@c : input data
+  *Return: c
   */
 char *cap_string(char *c)
 {
 	int index = 0;
 
-    	while (c[index] != '\0')
-    	{
-       		 if (c[index] == 32 || c[index] == 9 || c[index] == 44 || c[index] == '\n' || c[index] == ';' || c[index] == '.' || c[index] == '!' || c[index] == '?' || c[index] == '\"' || c[index] == '(' || c[index]  == ')' || c[index] == '{' || c[index] == '}')
-            	{
-               		 if (c[index +1] >= 'A' && c[index + 1] <= 'Z')
-                	{
+	while (c[index] != '\0')
+	{
+		if (c[index] == 32 || c[index] == '\t' || c[index] == '\n' || c[index] == ',' || c[index] == '.' || c[index] == ';' || c[index] == '!' || c[index] == '?' || c[index] == '"' || c[index] == '(' || c[index] == ')' || c[index] == '}' || c[index] == '{')
+		{
+			if (c[index + 1] >= 'A' && c[index + 1] <= 'Z')
+			{
+			}
+			else if (c[index + 1] >= 'a' && c[index + 1] <= 'z')
+			{
+				c[index + 1] = c[index + 1] - 32;
+			}
+		}
 
-                	}
-                	else if (c[index + 1] >='a' && c[index + 1] <= 'z')
-                	{
-                    		c[index + 1] = c[index + 1] - 32;
-                	}
-            	}
-
-            	index++;
+		index++;
 	}
 	return (c);
 }
