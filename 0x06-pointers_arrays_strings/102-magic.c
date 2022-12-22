4d802beb87a13ca6531323892a803e6b659207df
datsitsre <atsitsredaniel@gmail.com>
2022-12-22 23:28:04 +0000

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -12,16 +12,12 @@ int a[5];
 int *p;
 
 a[2] = 1024;
-p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
+p = &n; /* * - you are not allowed to use a
 * - you are not allowed to modify p
-* - only one statement
+* - only one statement */
+*(p + 2) = 98;/*
 * - you are not allowed to code anything else than this line of code
 */
-*(p + 2) = 98;
 ;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
