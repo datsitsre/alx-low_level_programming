a224450c117980972050999b1dd6668b0a82d3ea
datsitsre <atsitsredaniel@gmail.com>
2022-12-22 02:50:16 +0000

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@ void reverse_array(int *a, int n)
 	int index;
 	int temp;
 
-	for (index = 0; index < (n /2); index++)
+	for (index = 0; index < (n / 2); index++)
 	{
 		temp = a[index];
 		a[index] = a[n - index - 1];
