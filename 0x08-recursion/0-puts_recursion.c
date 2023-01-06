c725b4e05991214dd21003d7a040963e131073f4
datsitsre <atsitsredaniel@gmail.com>
2023-01-06 12:20:26 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
-/** 
+/**
   *_puts_recursion - prints a string
   *@s: string to use '
   *Return : 0
   */
 void _puts_recursion(char *s)
 {
-
-	while(*s)
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
 	{
 		_putchar(*s);
-		s++;
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+
 }
