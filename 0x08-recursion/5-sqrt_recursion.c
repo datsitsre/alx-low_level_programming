8c803f1616d662e402e4fba41427b7cee7fd8861
datsitsre <atsitsredaniel@gmail.com>
2023-01-06 16:04:05 +0000

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  *_sqrt_recursion - natural square of a number
+  *@n: natural number
+  *Return: n
+  */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+
+	return (square_root_wrapper(n, 1));
+}
+
+/**
+  *square_root_wrapper - the naturl root
+  *@n: number of n
+  *@min:  mini
+  *@max: maximum
+  *Return: 0
+  */
+int square_root_wrapper(int min, int max)
+{
+
+	if (max * max == min)
+		return (max);
+
+	if  ( max * max > min)  
+		return (-1);	
+
+	return (square_root_wrapper(min, max + 1));
+}
