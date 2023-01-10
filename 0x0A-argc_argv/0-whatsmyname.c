e1881f548200ca57fb869db221a016a2f6e835f5
datsitsre <atsitsredaniel@gmail.com>
2023-01-10 03:01:28 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - program that prints its name
+  *@argc: the argument count
+  *@argv: the input argument
+  *Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+
+	return (0);
+
+}
