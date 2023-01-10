d15af1f91f0e5d5a1dcd5b89c96c0afab2ff3eda
datsitsre <atsitsredaniel@gmail.com>
2023-01-10 14:38:18 +0000

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,8 +11,8 @@
   */
 int main(int argc, char *argv[])
 {
-	int index;
 	int num_1;
+	int num_2;
 	int result = 1;
 
 	if (argc < 2)
@@ -20,17 +20,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		for (index = 1; index < argc; index++)
-		{
-			num_1 = strtol(argv[index], NULL, 10);
-			result = result * num_1;
-		}
+
+	num_1 = strtol(argv[1], NULL, 10);
+	num_2 = strtol(argv[2], NULL, 10);
+	result = num_2 * num_1;
 
 		printf("%d\n", result);
 
 	return (0);
-	}
 
 }
