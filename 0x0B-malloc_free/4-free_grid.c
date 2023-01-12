0e4f749d7a19eedb53e9d335f48dd2f5434fcef8
datsitsre <atsitsredaniel@gmail.com>
2023-01-12 23:16:34 +0000

    4. It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *free_grid - frees a 2 dimensionsional grid previously
+  *@grid: CREATE TABLE
+  *@height: the columns
+  *Return: pointer
+  */
+void free_grid(int **grid, int hieght)
+{
+	int index_1; 
+	for (index_1 = 0; index_1 < height;index_1++)
+	{
+		free(grid[index_1]);
+	}
+	free(grid);
+
+}
