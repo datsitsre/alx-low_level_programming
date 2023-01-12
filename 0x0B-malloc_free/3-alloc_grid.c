e6b25e8f7a5b4419feab78c2d68ff911ab6fce3e
datsitsre <atsitsredaniel@gmail.com>
2023-01-12 22:36:39 +0000

    dimensional array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,22 +13,25 @@ int **alloc_grid(int width, int height)
 	int **ptr;
 	int index_1, index_2;
 
-	ptr = malloc(sizeof(int) * width * height);
+	ptr = malloc(sizeof(int *) *  height);
 	
-	if (width < 1 || height < 1)
-		return (NULL);
 
 	if (ptr == NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 
 	for (index_1 = 0; index_1 < height; index_1++)
 	{
+		ptr[index_1] = (int *)malloc(width * sizeof(int));
+	}
+
+
+	for (index_1 = 0; index_1 < height; index_1++)
+	{
 		for (index_2 = 0; index_2 < width; index_2++)
 		{
-			ptr[width][height] = 0;
+			ptr[index_1][index_2] = 0;
 		}
 	}
 
