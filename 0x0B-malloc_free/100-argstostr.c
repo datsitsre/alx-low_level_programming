6c0fc61a67d855654352310e27b87a016a90cc72
datsitsre <atsitsredaniel@gmail.com>
2023-01-13 01:23:38 +0000

    It isn't the mountain

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,11 +11,13 @@ char *argstostr(int ac, char **av)
 {
 
 	char *ptr;
-	int index_1, index_2;
+	int index_1, index_2, ptr_int;
 	int len;
 
+	if (ac == 0)
+		return (NULL);
 
-	for (index_1 = 0; index_1 < ac; index_1++)
+	for (index_1 = len = 0; index_1 < ac; index_1++)
 	{
 		if (av[index_1] == NULL)
 			return (NULL);
@@ -24,26 +26,30 @@ char *argstostr(int ac, char **av)
 		{
 			len++;
 		}
+		len++;
 	}
 
 	ptr = malloc(sizeof(char) * (len + 1));
 
 	if (ptr == NULL)
-		return (NULL);
-
-	for (index_1 = 0; index_1 < len; index_1++)
 	{
-		for (index_2 = 0; index_2 < len; index_2++)
-		{
-			if (av[index_1][index_2] == '\0')
-			{
-				ptr[index_1] = '\n';
-			}
-			if (index_1 < len - 1)
-				ptr[index_1] = av[index_1][index_2];
-		}
+		free(ptr);
+		return (NULL);
 	}
-	ptr[index_1] = '\0';
+
+	for (index_1 = index_2 = ptr_int = 0; ptr_int < len; ptr_int++, index_2++)
+	{
+		if (av[index_1][index_2] == '\0')
+		{
+			ptr[ptr_int] = '\n';
+			index_1++;
+			ptr_int++;
+			index_2 = 0;
+		}
+		if (ptr_int < len - 1)
+			ptr[ptr_int] = av[index_1][index_2];
+	}
+	ptr[ptr_int] = '\0';
 
 	return (ptr);
 }
