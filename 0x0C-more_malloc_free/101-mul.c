4ca85c32233b8574987c69312893f4b731656b4f
datsitsre <atsitsredaniel@gmail.com>
2023-01-13 16:35:21 +0000

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *main - multiple two numbers
+  *@argc: count argc
+  *@argv: number of argument
+  *Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int index_1, index_2;
+	int result;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (index_1 = 0; index_1 < argc; index_1++)
+	{
+		for (index_2 = 0; index_2 < '\0'; index_2++)
+		{
+			if (argv[index_1][index_2] > 57 || argv[index_1][index_2])
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
+	result = atol(argv[2]) * atol(argv[1]);
+	printf("%d\n", result);
+
+	return (0);
+
+}
