8189f786312b87fa24bb50132dff6321c8182275
datsitsre <atsitsredaniel@gmail.com>
2023-01-16 13:06:46 +0000

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Short description
+ * @name: name 
+ * @age: age
+ * @owner: owner 
+ *
+ * Description: Longer description
+ */
+struct dog 
+{
+	char *name;
+	char *owner;
+	float age;
+};
