8c6c4912abe42a6422d6587eb1831db02cdbc0b5
datsitsre <atsitsredaniel@gmail.com>
2023-01-26 23:29:43 +0000

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+  *list_len - list the number of elements
+  *@h: head points
+  *Return: 0
+  */
+size_t list_len(const list_t *h)
+{
+	size_t index;
+
+	for (index = 0;h;h = h->next)
+	{
+		index++;
+	}
+	return (index);
+}
