aa6853f34db23abc80216b89fc36cca14a20df97
datsitsre <atsitsredaniel@gmail.com>
2023-01-30 16:04:48 +0000

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,17 +5,19 @@
 
 /**
   *print_listint - print the int part of the list
-  *h: the head pointer;
+  *@h: the head pointer;
   *Return: n or null
   */
 size_t print_listint(const listint_t *h)
 {
 	size_t index;
+
 	if (h == NULL)
-		return(0);
-	for (index = 0; h;h =  h->next)
+		return (0);
+
+	for (index = 0; h; h =  h->next)
 	{
-		printf("%d\n",h->n);
+		printf("%d\n", h->n);
 		index++;
 	}
 
