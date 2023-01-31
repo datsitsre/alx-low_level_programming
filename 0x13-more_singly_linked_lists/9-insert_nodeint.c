2ce656e39113355cedce19a4a2ef742b7f8199da
datsitsre <atsitsredaniel@gmail.com>
2023-01-31 16:44:33 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,42 +12,38 @@
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t **ptr;
+	listint_t *ptr = *head;
 	listint_t *ptr_1 = malloc(sizeof(listint_t));
-	int count = 0;
-	int num = idx;
+	int index = idx;
+	int count;
 
+	if (!ptr_1 || !ptr)
+		return (NULL);
 
-	ptr = head;
 	ptr_1->n = n;
 	ptr_1->next = NULL;
 
-	if (!*ptr)
-		return (NULL);
-	if (!ptr_1)
-		return (NULL);
-
-	if (num == 0)
+	if (index == 0)
 	{
-		ptr_1->next = *ptr;
-		*ptr = ptr_1;
+		ptr_1->next = ptr;
+		ptr = ptr_1;
 		return (ptr_1);
 	}
 
-	while (*ptr && count < num)
+
+	for (count = 0; ptr && count < index; count++)
 	{
-		if (count == num - 1)
+		if (count == index - 1)
 		{
-			ptr_1->next = (*ptr)->next;
-			(*ptr)->next = ptr_1;
+			ptr_1->next = ptr->next;
+			ptr->next = ptr_1;
 			return (ptr_1);
 		}
 		else
-			(*ptr) = (*ptr)->next;
-
-	count++;
+		{
+			ptr = ptr->next;
+		}
 	}
 
 	return (NULL);
-
 }
