9c549ce448a241a7a3122df2ff23f603aac55606
datsitsre <atsitsredaniel@gmail.com>
2023-02-02 16:04:07 +0000

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+  *get_bit - print the representation of a Number
+  *@n: n
+  *Return:0
+  */
+void print_binary(unsigned long int n)
+{
+	int index, count = 0;
+	unsigned long int store;
+
+	for (index = 63; index >= 0; index--)
+	{
+		store = n >> index;
+
+		if (store & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
