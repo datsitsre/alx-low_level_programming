a3e441d069be167beadeb891bba0e566b30634b8
datsitsre <atsitsredaniel@gmail.com>
2023-02-06 16:51:57 +0000

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+  *read_textfile - read a text file
+  *@filename: the filename
+  *@letters: the letters
+  *Return: 0 or letters
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	ssize_t open_data, read_data, write_data;
+	char *data_buffer;
+
+
+	if (filename == NULL)
+		return (0);
+
+
+	data_buffer = malloc(sizeof(char) * letters);
+
+	if (data_buffer == NULL)
+		return (0);
+
+	open_data = open(filename, O_RDONLY);
+	read_data = read(open_data, data_buffer, letters);
+	write_data = write(STDOUT_FILENO, data_buffer, read_data);
+
+	if (read_data == -1 ||
+			write_data == -1 || write_data != read_data || open_data == -1)
+	{
+		free(data_buffer);
+		return (0);
+	}
+	free(data_buffer);
+
+	return (write_data);
+
+
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
