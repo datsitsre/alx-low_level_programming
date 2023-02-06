835764f28ba6621ebbe1ba82b6cfd06372776484
datsitsre <atsitsredaniel@gmail.com>
2023-02-06 22:17:55 +0000

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+  *create_file - a create file
+  *@filename: file name
+  *@text_content: file contents
+  *Return: 0 or file created
+  */
+int create_file(const char *filename, char *text_content)
+{
+
+	int filedata;
+	int writedat;
+	int num = strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	filedata = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (filedata < 0)
+		return (-1);
+	else
+		return (1);
+
+	if (text_content)
+	{
+		writedat = write(filedata, text_content, num);
+		if (writedat != num)
+			return (-1);
+	}
+	close(filedata);
+	return (1);
+}
